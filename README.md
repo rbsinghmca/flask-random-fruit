082cd7a3d4b1db3c0867588a11c67630555eb14a
rbsinghmca <50779409+rbsinghmca@users.noreply.github.com>
2023-08-28 09:59:30 +0000

    Random fruit application is ready

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # flask-random-fruit
+
+A flask microservice for generating random fruit
+
+Create virtualenv:  `virtualenv ~/.venv && source ~/.venv/bin/activate`
+Install:  `make all`
+Run:  `python app.py`
---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import render_template
+from random import choices
+
+app = Flask(__name__)
+
+def random_fruit():
+    """Return random fruit"""
+
+    fruits = ["apple", "mangoe", "banana"]
+    return choices(fruits)
+
+@app.route("/")
+def fruit():
+    """Return random fruit"""
+
+    my_fruit = random_fruit()
+    return render_template("index.html", title="Random Fruit", fruit = my_fruit)
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
